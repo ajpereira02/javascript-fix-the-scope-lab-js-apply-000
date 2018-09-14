eab3b8daace0a0aa28c404be12a6ef3119167d8b
Ashley Pereira <ashleypereira70@gmail.com>
2018-09-14 17:36:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,29 +1,34 @@
-var animal = 'dog'
+var animal = "dog"
 
 function myAnimal() {
-  return animal
+return animal
 }
+//A function can access all variables defined from the outside the function
 
 function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
+  var animal = 'cat'
   return animal
 }
+//A function can access all variables defined inside the function
 
+var n = 193
 function add2(n) {
+    const two = 2
   return n + two
 
   // Feel free to move things around!
-  const two = 2
 }
 
-var funkyFunction = function() {
-  return function() {
-    return "FUNKY!"
-  }
+var theFunk = "FUNKY!"
+  function funkyFunction() {
+  return theFunk
+
 }
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
-var theFunk = funkyFunction
+console.log(funkyFunction)
+
